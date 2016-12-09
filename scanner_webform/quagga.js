e1cc4344aed28bebadb4c6aadb90b60c3b1cbfd0
SophieDurrant <sgd38@cam.ac.uk>
2016-12-09 22:36:41 +0000

    made form stop when detected

--- scanner_webform/quagga.js
+++ scanner_webform/quagga.js
@@ -156,13 +156,41 @@
 
 });*/
 
+/*Init code is adapted from code at https://github.com/serratus/quaggaJS
+ * 
+ * This code is licensed with the MIT License.
+ * 
+ * 
+The MIT License (MIT)
+
+Copyright (c) 2014 Christoph Oberhofer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 Quagga.init({
     inputStream : {
       name : "Live",
       type : "LiveStream"
     },
     decoder : {
-      readers : ["ean_reader"]
+      readers : ["ean_reader", "upc_reader"]
     }
   }, function(err) {
       if (err) {
@@ -183,6 +211,7 @@ Quagga.init({
     Quagga.onDetected(function(result) {
         var code = result.codeResult.code;
         document.getElementById("code").value=code;
+        Quagga.stop();
     });
 
 
