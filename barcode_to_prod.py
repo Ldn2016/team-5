2958fd5ff406077ede02c5640daeec80eb945a9f
Joe Malt <joemalt1832@gmail.com>
2016-12-09 22:19:19 +0000

    Tidy up barcode-to-prod code

--- barcode_to_prod.py
+++ barcode_to_prod.py
@@ -1,22 +1,45 @@
 import requests
 import json
+import re
 
-#todo: include get_book_data from Phoebe
 
-def get_generic_product_data(barcode)
-    r = requests.get("https://www.buycott.com/upc/%s/" % barcode)
+#todo: include get_book_data() from Phoebe
+def get_book_data(isbn):
+    #isbn inputted as string
+    address = "http://isbndb.com/api/v2/json/NAZ4ZQZO/book/"+isbn
+    r = requests.get(address)
+    data = json.loads(r.text)
+    info_to_return = {}
+    given_info = data.get("data")[0]
+    info_to_return["author"] = given_info.get("author_data")[0].get("name")
+    info_to_return["title"] = given_info.get("title_latin")
+    return (info_to_return)
+    
+def get_generic_product_data(barcode):
+    url = "http://opengtindb.org/index.php?cmd=ean1&ean=" + barcode + "&sq=1"
+    print(url)
+    r = requests.get(url)
+    
     text = r.text
+    data = re.findall('NAME=\"fullname\" VALUE=\"(.*?)\">', text, re.DOTALL)
+    if len(data) > 0:
+        return data[0]
+    else:
+        return None
 
 
-print(get_generic_product
-if is_isbn(barcode):
-    data = get_book_data(barcode)
-    #do any processing necessary
-    return data
-else:
-    data = get_generic_product_data(barcode)
-    #do some processing
-    return data
+print(get_generic_product_data("7622300743536"))
+
+
+#if is_isbn(barcode):
+#    data = get_book_data(barcode)
+#    #do any processing necessary
+#    return data
+  
+#else:
+#    data = get_generic_product_data(barcode)
+#    #do some processing
+#    return data
 
 def is_isbn(isbn):
     if (len(isbn) != 10) and (len(isbn) != 13):
