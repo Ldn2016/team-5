d989d593cecd8ba93113d277add05af76f77b91c
Roddy <rlm72@cam.ac.uk>
2016-12-09 20:54:38 +0000

    Made models display nicely

--- cataloguesite/catalogue/models.py
+++ cataloguesite/catalogue/models.py
@@ -4,19 +4,31 @@ from django.db import models
 class Item(models.Model):
     id_number = models.IntegerField()
 
+    def __str__(self):
+        return str(self.id_number)
+
 
 class Book(Item):
     title = models.CharField(max_length=1000)
     author = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
 
 
 class CD(Item):
     title = models.CharField(max_length=1000)
     artist = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.title
+
 
 class Store(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
 
 
 class Quantity(models.Model):
