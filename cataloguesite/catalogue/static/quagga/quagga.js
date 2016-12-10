ed5b9735b8d86e802c09cb724e18d82ababeba62
SophieDurrant <sgd38@cam.ac.uk>
2016-12-10 03:27:49 +0000

    added validator to quagga thing

--- cataloguesite/catalogue/static/quagga/quagga.js
+++ cataloguesite/catalogue/static/quagga/quagga.js
@@ -51,17 +51,66 @@ Quagga.init({
   });
 
     Quagga.onDetected(function(result) {
+        //only registers if isbn valid
         var code = result.codeResult.code;
-        document.getElementById("code").value=code;
-        localStorage.setItem('barcode', code);
-        Quagga.stop();
-        $.post("bookpost", localStorage.getItem("barcode"), function(data)
+        if (isValidIsbn(code)) {
+            document.getElementById("code").value=code;
+            localStorage.setItem('barcode', code);
+            Quagga.stop();
+            $.post("bookpost", localStorage.getItem("barcode"), function(data)
                 {document.open(); 
                 document.write(data);
                 document.close();} );
+        }
 
         
     });
+    
+    //this is inspired by code on stack overflow
+    var isValidIsbn = function(str) {
 
+    var sum,
+        weight,
+        digit,
+        check,
+        i;
+
+    str = str.replace(/[^0-9X]/gi, '');
+
+    if (str.length != 10 && str.length != 13) {
+        return false;
+    }
+
+    if (str.length == 13) {
+        sum = 0;
+        for (i = 0; i < 12; i++) {
+            digit = parseInt(str[i]);
+            if (i % 2 == 1) {
+                sum += 3*digit;
+            } else {
+                sum += digit;
+            }
+        }
+        check = (10 - (sum % 10)) % 10;
+        return (check == str[str.length-1]);
+    }
+
+    if (str.length == 10) {
+        weight = 10;
+        sum = 0;
+        for (i = 0; i < 9; i++) {
+            digit = parseInt(str[i]);
+            sum += weight*digit;
+            weight--;
+        }
+        check = 11 - (sum % 11);
+        if (check == 10) {
+            check = 'X';
+        }
+        return (check == str[str.length-1].toUpperCase());
+    }
+}
+
+    
 
     
