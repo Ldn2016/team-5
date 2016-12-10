8deea5ec9db33db22f9a368f758fa5832adcbd1a
SophieDurrant <sgd38@cam.ac.uk>
2016-12-10 09:45:23 +0000

    commented

--- cataloguesite/catalogue/views.py
+++ cataloguesite/catalogue/views.py
@@ -6,15 +6,28 @@ import requests
 import json
 import re
 
+"""This page stores how Django processes the input on each webpage"""
+
 isbn_lookup_base = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
 
+@csrf_exempt
+def book_post(request):
+    "Takes the POST data from the scanner page and puts the data on the confirmation page"
+    try:
+        print(request.POST)
+        data = get_book_data(request.POST['isbn'])
+        data['store'] = request.POST['store']
+        return render(request, 'catalogue/product_scan_check.html', {'info' : data})
+    except KeyError:
+        return render(request, 'catalogue/product_scanner.html')
+
 def get_book_data(isbn):
+    "Looks up the ISBN on the Google API and returns the title, author and thumbnail"
     #isbn inputted as string
     address = isbn_lookup_base + isbn
     r = requests.get(address)
     data = json.loads(r.text)
     info_to_return = {}
-    print(data)
     info_to_return["isbn"] = isbn
     info_to_return["title"] = data['items'][0]['volumeInfo']['title']
     info_to_return["author"] = data['items'][0]['volumeInfo']['authors'][0]
@@ -23,6 +36,7 @@ def get_book_data(isbn):
 
 @csrf_exempt
 def product_enter(request):
+    "Submits the request to the database via POST"
     print(request.POST)
     book_title = request.POST['title']
     book_author = request.POST['author']
@@ -35,30 +49,6 @@ def product_enter(request):
     quantity.amount += 1
     quantity.save()
     return render(request, 'catalogue/thank_you.html')
-
-
-def add_item(item_dict):
-    """
-    Add JSON objects to the database in the correct way.
-
-    JSON objects should be formatted as so:
-        {
-            "store": store,
-            "amount": amount,
-            "object": {
-                "title": title,
-                etc.
-            }
-        }
-    Add an item to the directory.
-    `item_dict` should be a Python dict, not JSON.
-    """
-    new_item, _ = Book.objects.get_or_create(**item_dict["object"])
-    store, _ = Store.objects.get_or_create(name=item_dict["store"])
-    quantity, _ = Quantity.objects.get_or_create(item=new_item, store=store,
-                                                 defaults={"amount": 0})
-    quantity.amount += 1
-    quantity.save()
 
 
 def book_detail(request, book_id):
@@ -93,27 +83,22 @@ def store_detail(request, store_id):
 
 
 def search(request):
+    "Returns search form"
     return render(request, 'catalogue/search.html')
 
 
 def barcode_scanner(request):
+    "Takes user to page where they can scan in a product."
     return render(request, 'catalogue/product_scanner.html')
 
 def contact_page(request):
+    "Returns contact page"
     return render(request, 'catalogue/contact_page.html')
-
-@csrf_exempt
-def book_post(request):
-    try:
-        print(request.POST)
-        data = get_book_data(request.POST['isbn'])
-        data['store'] = request.POST['store']
-        return render(request, 'catalogue/product_scan_check.html', {'info' : data})
-    except KeyError:
-        return render(request, 'catalogue/product_scanner.html')
 
 
 def search_by_title(request):
+    """Performs search function - takes the query on the form on a search page (via get request) and returns a page
+    which displays books whose titles contain the search request. """
     query = request.GET.get('q', '')
     results = Book.objects.filter(title__contains = query)
     return render(request, 'catalogue/search_results.html', {'results' : results})                       
